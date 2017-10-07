563441f7258e22cdd0d6c6a869f5412dd88e51d1
nachosoto93 <nachosoto.93@gmail.com>
2017-10-07 00:46:01 -0300

    REfactor V0.1

--- src/main/java/com/simpleavrokafka/SimpleAvroKafkaProducer.java
+++ src/main/java/com/simpleavrokafka/SimpleAvroKafkaProducer.java
@@ -1,4 +1,5 @@
 package com.simpleavrokafka;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
@@ -16,70 +17,60 @@ import com.twitter.bijection.avro.GenericAvroCodecs;
 
 public class SimpleAvroKafkaProducer {
 
-	public static void main(String[] args) throws InterruptedException, IOException {
-		
-		
-		// CHECK VALIDATIONS HERE. ADD EXCEPTION
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		if (args.length > 4 || args.length < 3) {
+			System.out.println("Please, check the right syntax on README file");
+			return;
+		}
+
 		File file = new File(args[0]);
-		
-		// CODE A CONSTRUCTOR HERE
+
+		Properties props = kakfaPropertiesConstructor(args[1]);
+		try {
+
+			Boolean defaultLoop = new Boolean(false);
+			
+			if (args.length == 4 && args[3].equals("-loop"))
+				defaultLoop = true;
+
+			do {
+				readCompleteAvroFile(args[2], file, props);
+				System.out.println("--------------------EOF---------------------\n");
+				Thread.sleep(15000);
+			} while (defaultLoop);
+			System.out.println("EOF");
+
+		} catch (IOException e) {
+			System.out.println(e);
+		} 
+
+	}
+
+	private static Properties kakfaPropertiesConstructor(String bootstrapServerName) {
 		Properties props = new Properties();
-		props.put("bootstrap.servers", args[1]);
+		props.put("bootstrap.servers", bootstrapServerName);
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
+		return props;
+	}
 
-		
-		
-		
+	private static void readCompleteAvroFile(String topicName, File file, Properties props) throws IOException {
 		
 		GenericDatumReader<GenericData.Record> datum = new GenericDatumReader<GenericData.Record>();
-		
-		
 		DataFileReader<GenericData.Record> reader = new DataFileReader<GenericData.Record>(file, datum);
 		Schema schema = reader.getSchema();
-
-
 		GenericData.Record record = new GenericData.Record(schema);
 		Injection<GenericRecord, byte[]> recordInjection = GenericAvroCodecs.toBinary(schema);
 		KafkaProducer<String, byte[]> producer = new KafkaProducer<String, byte[]>(props);
-		Boolean defaultLoop = new Boolean(false);
 		
-		
-		/* Re-factor, with no duplicated code and you'll create a class */
-		
-		
-		if(args.length==4 && args[3].equals("-loop")) defaultLoop=true;
-		
-		if(defaultLoop){
-			while(true){
-				reader = new DataFileReader<GenericData.Record>(file, datum);
-				while (reader.hasNext()) {
-					reader.next(record);
-					byte[] bytes = recordInjection.apply(record);
-					ProducerRecord<String, byte[]> theRecord = new ProducerRecord<String, byte[]>(args[2], bytes);
-					producer.send(theRecord);
-				}
-				System.out.println("--------------------EOF---------------------\n");
-				Thread.sleep(1000);
-				
-			}
+		while (reader.hasNext()) {
+			reader.next(record);
+			byte[] bytes = recordInjection.apply(record);
+			ProducerRecord<String, byte[]> theRecord = new ProducerRecord<String, byte[]>(topicName, bytes);
+			producer.send(theRecord);
 		}
-		else {
-			reader = new DataFileReader<GenericData.Record>(file, datum);
-			
-			while (reader.hasNext()) {
-				reader.next(record);
-				byte[] bytes = recordInjection.apply(record);
-				ProducerRecord<String, byte[]> theRecord = new ProducerRecord<String, byte[]>(args[2], bytes);
-				producer.send(theRecord);
-			}
-			System.out.println("EOF");
-			reader.close();
-			producer.close();
-			
-		}
-			
-	
-		
+		reader.close();
+		producer.close();
 	}
 }
